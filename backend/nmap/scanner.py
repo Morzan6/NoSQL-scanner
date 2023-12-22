8bade51575917b5377d3fe5557ed0c4d75977ad7
CyberFather <maks12ate@gmail.com>
2023-12-22 23:45:08 +0300

    Delete useless code and add port checking

--- backend/nmap/scanner.py
+++ backend/nmap/scanner.py
@@ -1,10 +1,12 @@
-
-
-class Scannner(object):
+class Scannner:
     """
     Scanner class for nmap
     """
     def __init__(self, ip: str, port: int, **kwargs):
+
+        if not (0 <= port <= 65535):
+            raise ValueError("Port must be between 0 and 65535")
+
         self.ip = ip
         self.port = port
         self.creds = kwargs
